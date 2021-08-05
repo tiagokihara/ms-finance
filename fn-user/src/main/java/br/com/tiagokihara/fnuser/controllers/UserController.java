7be9f88eb445759632d9654d6e8ab33a34568b86
Tiago <tiagokihara@gmail.com>
2021-08-05 09:06:19 -0300

    create endpoint

--- fn-user/src/main/java/br/com/tiagokihara/fnuser/controllers/UserController.java
+++ fn-user/src/main/java/br/com/tiagokihara/fnuser/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,11 @@ public class UserController {
 
 	@GetMapping(value = "/search")
 	public ResponseEntity<User> getByEmail(@RequestParam String email) {
+		return ResponseEntity.ok(userRepository.findByEmail(email).orElseThrow(IllegalArgumentException::new));
+	}
+
+	@PostMapping(value = "/")
+	public ResponseEntity<User> createEmail(@RequestParam String email) {
 		return ResponseEntity.ok(userRepository.findByEmail(email).orElseThrow(IllegalArgumentException::new));
 	}
 }
